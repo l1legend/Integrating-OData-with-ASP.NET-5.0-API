38957bb9a448b0e801635c99cc30d90bf0bc2987
minTwin <michaelmin2020@gmail.com>
2021-12-22 13:05:16 -0600

    Commit
    Add initial Starting Files and text note for url commands in browser.

---
+++ ODataWithDotNet5Demo/ODataWithDotNet5Demo/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using ODataWithDotNet5Demo.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace ODataWithDotNet5Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            return Ok(new List<Student>
+            {
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "MM",
+                    Grade = 100
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "MM2",
+                    Grade = 200
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "MM3",
+                    Grade = 999
+                }
+            });
+        }
+    }
+}
